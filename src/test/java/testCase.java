a1b7c62f9ca39cd70021ea330364448e04e21467
bereket <bkimesfin12@gmail.com>
2023-02-25 23:35:43 -0600

    git

---
+++ src/test/java/testCase.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class testCase {
+
+public void DrugAndDrop() {
+
+WebDriver driver = new ChromeDriver();
+
+}
+}
